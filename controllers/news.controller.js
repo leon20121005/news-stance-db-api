2657116ee696f37664a2d5c3afd3a306af7b722d
leon20121005 <leon20121005@gmail.com>
2019-05-01 01:45:44 +0800

    fix the json format of http response

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -2,19 +2,26 @@ const News = require('../models/news.model');
 
 exports.index = function(request, response)
 {
+    var is_first_object = true;
     response.setHeader('Content-Disposition', 'attachment; filename=news.json');
     response.setHeader('Content-Type', 'application/json');
     News.find().cursor()
     .on('data', function(document)
     {
+        var prefix = is_first_object ? '[' : ',';
         const data = JSON.stringify(document);
-        response.write(data, function(error)
+        response.write(prefix + data, function(error)
         {
             return console.error(error);
         });
+        is_first_object = false;
     })
     .on('end', function()
     {
+        response.write(']', function(error)
+        {
+            return console.error(error);
+        })
         response.end();
     });
 };
